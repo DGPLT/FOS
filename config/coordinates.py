b2e82d8c52579bbb8f9021543ad32fa379c73d29
irack000 <IRACK000@gmail.com>
2023-05-16 07:47:28 +0900

    Feat: Change json file to py file
    So that we can simply import json

---
+++ config/coordinates.py
@@ -0,0 +1,47 @@
+coordinates = {
+    "Targets": {
+        "T1": {
+            "Latitude": 280, "Longitude": 20
+        },
+        "T2": {
+            "Latitude": 280, "Longitude": 60
+        },
+        "T3": {
+            "Latitude": 280, "Longitude": 100
+        },
+        "T4": {
+            "Latitude": 240, "Longitude": 20
+        },
+        "T5": {
+            "Latitude": 240, "Longitude": 60
+        },
+        "T6": {
+            "Latitude": 240, "Longitude": 100
+        },
+        "T7": {
+            "Latitude": 200, "Longitude": 20
+        },
+        "T8": {
+            "Latitude": 200, "Longitude": 60
+        },
+        "T9": {
+            "Latitude": 200, "Longitude": 100
+        }
+    },
+    "Lakes": {
+        "L1": {
+            "Latitude": 270, "Longitude": 270
+        }
+    },
+    "Bases": {
+        "A": {
+            "Latitude": 100, "Longitude": 85
+        },
+        "B": {
+            "Latitude": 80, "Longitude": 190
+        },
+        "C": {
+            "Latitude": 140, "Longitude": 230
+        }
+    }
+}
