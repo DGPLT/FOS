1128c633767cb449300ee0e67e59cc479d76a46d
irack000 <IRACK000@gmail.com>
2023-05-17 08:12:30 +0900

    Chore: Add discription

--- src/simulator/app.py
+++ src/simulator/app.py
@@ -14,7 +14,7 @@ from api.api_resolver import ApiResolver
 def run_simulator(host="", port=0, visualize=True, logging=True):
     if not host:
         host = input("Please specify a host to connect to: ")
-    
+
     if not port:
         port = int(input("Please set a port of the server to connect to: "))
 
@@ -33,7 +33,7 @@ def run_simulator(host="", port=0, visualize=True, logging=True):
                 # Run the game
                 if not await scenario.run_game(api, visualizer):
                     break
-                visualizer.clock.tick(60)
+                visualizer.clock.tick(60)  # Set FPS 60
                 main(visualizer=visualizer, *args, **kwargs)
             # Disconnect from the server
             await api.disconnect()
