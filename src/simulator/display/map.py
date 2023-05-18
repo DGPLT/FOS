7d85bf6b1811dd716933ea233a1af0d143db4dc8
Bigtoad1 <93104726+Bigtoad1@users.noreply.github.com>
2023-05-18 22:59:22 +0900

    Add files via upload
    Fire positions are bugged but this is what I have working, Aircraft will be similarly updated as a sprite group.

--- src/simulator/display/map.py
+++ src/simulator/display/map.py
@@ -6,33 +6,25 @@ Description : Game MAP Design
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 PIXEL_EXPANSION = 2.3
 MAX_CORDINATION = 300
-SCREEN_WIDTH = MAX_CORDINATION * PIXEL_EXPANSION
-SCREEN_HEIGHT = MAX_CORDINATION * PIXEL_EXPANSION
+SCREEN_WIDTH = int(MAX_CORDINATION * PIXEL_EXPANSION)
+SCREEN_HEIGHT = int(MAX_CORDINATION * PIXEL_EXPANSION)
 
-
-# list of static objects (not moving)
-background = None
-lake = None
-base_a = None
-base_b = None
-base_c = None
-#TODO: Add 
-
+""" will make this a class attribute in visualize
 # list of dynamic objects
 fire = []  # [fire1, fire2, fire3, ...]
 aircraft = {}  # {"D1-A": _, ...}
+"""
 
 # Cordinates
-from config.coordinates import coordinates
+# from config.coordinates import coordinates
 
-
+""" map loading inside components
 def load_map(pygame):
-    """ Load map objects from resource files (Initialize) """
+     "Load map objects from resource files (Initialize)"
     load = pygame.image.load
 
     # static objects
-    global background, lake, base_a, base_b, base_c
-    background = load("res/background.png")
+    background = load("res/FOS_BACKGROUND.png")
     #.......
     #TODO: Load more and fix coordinates
 
@@ -40,4 +32,13 @@ def load_map(pygame):
     fire.extend([load("res/fire.png") for _ in range(9)])
     aircraft.update({})
     #TODO: Load more
+    
+"""
 
+class Aircraft(): # class for managing base aircraft
+
+    def __init__(self):
+        self.rotation = 0
+        self.latitude = 0
+        self.longitude = 0
+        self.image = None
