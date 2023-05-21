f562ef523ff33a6a80b6a1a13a20f4333f0b9c04
irack000 <IRACK000@gmail.com>
2023-05-21 18:41:42 +0900

    Feat: Update Game Scenario

--- src/simulator/app.py
+++ src/simulator/app.py
@@ -5,10 +5,11 @@ Coded with Python 3.10 Grammar by MUN, CHAEUN
 Description : 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 from functools import wraps
+import asyncio
 
-from display.components import GameVisualizer
-from round.scenario import GameScenarios
-from api.api_resolver import ApiResolver
+from src.simulator.display.components import GameVisualizer
+from src.simulator.round.scenario import GameScenarios
+from src.simulator.api.api_resolver import ApiResolver
 
 
 def run_simulator(host="", port=0, visualize=True, logging=True):
@@ -29,10 +30,20 @@ def run_simulator(host="", port=0, visualize=True, logging=True):
         async def wrapper(*args, **kwargs):
             # Connect to the server
             await api.connect()
+            # Start new round
+            await scenario.start_new_round(visualizer)
             while True:
                 # Run the game
                 if not await scenario.run_game(api, visualizer):
-                    break
+                    cur_round = scenario.current_round
+                    # Show Result Panel
+                    await visualizer.show_score_panel(cur_round.round_num, cur_round.is_win, cur_round.score)
+                    await asyncio.sleep(30)  # wait for 30 sec
+                    if cur_round.is_win and cur_round.round_num < 3:  # Win
+                        # Go next round
+                        await scenario.start_new_round(visualizer)
+                    else:  # Lose or Game Finished
+                        break
                 visualizer.clock.tick(60)  # Set FPS 60
                 main(visualizer=visualizer, *args, **kwargs)
             # Disconnect from the server
