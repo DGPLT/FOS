43bba176fcd15618c318c7275916832ab5fffca6
irack000 <IRACK000@gmail.com>
2023-05-21 20:04:21 +0900

    Refactor: Change Module Name

--- src/simulator/display/components.py
+++ src/simulator/display/components.py
@@ -4,7 +4,26 @@
 Coded with Python 3.10 Grammar by Waters, Nathaniel
 Description : Game CLI Log Console Interface & GUI Visualization
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-import map as maps
+_PIXEL_EXPANSION = 2.3
+_MAX_CORDINATION = 300
+_SCREEN_WIDTH = int(_MAX_CORDINATION * _PIXEL_EXPANSION)
+_SCREEN_HEIGHT = int(_MAX_CORDINATION * _PIXEL_EXPANSION)
+_SCREEN_SIZE = (_SCREEN_WIDTH, _SCREEN_HEIGHT)
+
+pygame = None
+load_image = None
+entities = None
+
+
+def load_pygame():
+    """ PyGame Loader """
+    global pygame, load_image, entities
+    if pygame is None:
+        import pygame as pygame_module
+        pygame = pygame_module
+        load_image = pygame.image.load
+        from . import entities as ent
+        entities = ent
 
 
 class GameVisualizer(object):
@@ -46,32 +65,16 @@ class GameVisualizer(object):
 
         if visualize:
             # import pygame library only when visualizer is enabled
-            import pygame
+            load_pygame()
 
             self.fire_group = pygame.sprite.Group()
 
-            class Fire(pygame.sprite.Sprite):
 
-                # Constructor. Pass in its x and y position
-                def __init__(self, width, height):
-                    # Call the parent class (Sprite) constructor
-                    pygame.sprite.Sprite.__init__(self)
-                    super().__init__()
-
-                    # Create an image of the fire
-                    # This could also be an image loaded from the disk.
-                    self.image = pygame.image.load("res/Fire1.png")
-
-                    # Fetch the rectangle object that has the dimensions of the image
-                    # Update the position of this object by setting the values of rect.x and rect.y
-                    self.rect = self.image.get_rect()
-                    self.rect.x = width
-                    self.rect.y = height
 
 
             temp = []
             for i in self.fire_sheet:
-                i = Fire(self.Targets[self.fire_sheet.index(i)][0], self.Targets[self.fire_sheet.index(i)][1])
+                i = entities.Fire(self.Targets[self.fire_sheet.index(i)][0], self.Targets[self.fire_sheet.index(i)][1])
                 temp.append(i)
             self.fire_sheet = temp
 
@@ -83,22 +86,19 @@ class GameVisualizer(object):
 
             pygame.init()
             self._set_round_caption(0)
-            self._screen = pygame.display.set_mode((maps.SCREEN_WIDTH, maps.SCREEN_HEIGHT))
+            self._screen = pygame.display.set_mode(_SCREEN_SIZE)
             
             # load map and other images
             self.background_i = pygame.image.load("res/FOS_BACKGROUND.png")
-            self.background_i = pygame.transform.scale(self.background_i, (maps.SCREEN_WIDTH, maps.SCREEN_HEIGHT))
+            self.background_i = pygame.transform.scale(self.background_i, _SCREEN_SIZE)
             self.drone_i = pygame.image.load("res/drone1.png")
             self.heli_i = pygame.image.load("res/heli1.png")
             self.plane_i = pygame.image.load("res/plane1.png")
             self.fire_i = pygame.image.load("res/Fire1.png")
 
             self.assign_aircraft(self.air_sheet) # assign class to each aircraft
-            #open the window
-            self.is_quit_pressed()
 
-
-    def is_quit_pressed(self) -> bool: # request to rename to window_start
+    def is_quit_pressed(self) -> bool:
         """ Check if game quit operation is ordered. """
         while self.visualize:
             self._screen.blit(self.background_i, (0, 0))
@@ -109,19 +109,18 @@ class GameVisualizer(object):
             self._display_update()
         self._pygame.quit()
 
+    def _set_round_caption(self, round_num: int):
+        self._pygame.display.set_caption("Fire Operation Simulator - Round " + str(round_num))
 
-    def _set_round_caption(self, round: int):
-        self._pygame.display.set_caption("Fire Operation Simulator - Round " + str(round))
-
-    def set_round_mode(self, round: int, unit_table: dict, target_list: dict):
+    async def set_round_mode(self, round_num: int, unit_table: dict, target_list: dict):
         """ Set Round Mode
         Internally, this function do initialization job for the object coordinates # what does this mean? and unit tabe import?
         """
         if self.logging:
-            print(f"Round Mode is now updated to {round}")
+            print(f"Round Mode is now updated to {round_num}")
         
         if self.visualize: # i don't know why this would be done here
-            self._set_round_caption(round)  # update window name
+            self._set_round_caption(round_num)  # update window name
 
             # arrange aircrafts
             #TODO: write code for this 
@@ -131,29 +130,29 @@ class GameVisualizer(object):
 
             self._display_update()
 
-    def show_score_panel(self, round: int, score: int):
+    async def show_score_panel(self, round_num: int, is_win: bool, score: int):
         """ Show Score Panel when a round is finished """
         if self.logging:
-            print(f"Round {round} finished. Score is {score}.")
+            print(f"[{'WIN' if is_win else 'LOSE'}] Round {round_num} finished. Score is {score}.")
 
         if self.visualize: # i would need to make more images for this and caption text
             #TODO
 
             self._display_update()
 
-    def assign_aircraft(self, air_sheet):
+    async def assign_aircraft(self, air_sheet):
         temp = []
         for i in air_sheet:
             if "D" in i:
-                i = maps.Aircraft()
+                i = entities.Aircraft()
                 i.image = self.drone_i
                 temp.append(i)
             elif "H" in i:
-                i = maps.Aircraft()
+                i = entities.Aircraft()
                 i.image = self.heli_i
                 temp.append(i)
             elif "A" in i:
-                i = maps.Aircraft()
+                i = entities.Aircraft()
                 i.image = self.plane_i
                 temp.append(i)
             else:
@@ -175,6 +174,3 @@ class GameVisualizer(object):
         """ Move object to the given coordinates with asynchronized update operation """
         #TODO
         self._display_update()
-
-
-game = GameVisualizer()
